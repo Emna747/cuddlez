d12005cac8707e629d04166c9e5295b315dedddc
Emna <84625792+Emna747@users.noreply.github.com>
2022-12-31 18:15:19 +0200

    signup page

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Furniture from "./Furniture";
 import Learning from "./Learning";
 import Toys from "./Toys";
 import Footer from "./Footer";
-//import Signuppage from "./Signuppage";
+
 import ButtonBases from "./ButtonBases";
-//import Checkout from "./Checkout";
+
 import SmartToyOutlinedIcon from "@mui/icons-material/SmartToyOutlined";
 import AbcOutlinedIcon from "@mui/icons-material/AbcOutlined";
 import DiamondOutlinedIcon from "@mui/icons-material/DiamondOutlined";
@@ -16,7 +16,7 @@ import MainBanner from "./MainBanner";
 import { Typography, Container } from "@mui/material";
 import useStyles from "./styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Checkout from "./Checkout";
+
 function App() {
   const classes = useStyles();
   return (
